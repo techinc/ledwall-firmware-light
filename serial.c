542e0d4c924b6415b3314c1bb328e2fade15fafe
Nathan Zadoks <nathan@nathan7.eu>
2012-09-30 13:56:15 +0200

    fixed tab

--- serial.c
+++ serial.c
@@ -7,15 +7,15 @@ void main(){
     DDRB=0xFF;
 
     //initialise the USART
-	unsigned int ubrr=(F_CPU/16/BAUD-1);
-	UBRR0H=(unsigned char)(ubrr>>8);
-	UBRR0L=(unsigned char)ubrr;
-	UCSR0B=(1<<RXEN0)|(1<<TXEN0);
-	UCSR0C=(3<<UCSZ00);
+    unsigned int ubrr=(F_CPU/16/BAUD-1);
+    UBRR0H=(unsigned char)(ubrr>>8);
+    UBRR0L=(unsigned char)ubrr;
+    UCSR0B=(1<<RXEN0)|(1<<TXEN0);
+    UCSR0C=(3<<UCSZ00);
 
     //enable USART interrupts and turn on interrupts
-   	UCSR0B|=(1<<RXCIE0);
-	sei();
+    UCSR0B|=(1<<RXCIE0);
+    sei();
 
     //waste CPU cycles
     while(1);
